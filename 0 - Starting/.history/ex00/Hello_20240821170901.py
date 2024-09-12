7f53d9b8797642986598646ab59ce11fe82fa5f0
Nugah <fwong@BESS-f2r4s17.clusters.42paris.fr>
2024-09-12 17:39:30 +0200

    exo 02

--- 0 - Starting/.history/ex00/Hello_20240821170901.py
+++
@@ -1,9 +0,0 @@
-t_list = ["Hello", "tata!"]
-ft_tuple = ("Hello", "toto!")
-ft_set = {"Hello", "tutu!"}
-ft_dict = {"Hello" : "titi!"}
-#your code here
-print(ft_list)
-print(ft_tuple)
-print(ft_set)
-print(ft_dict)
